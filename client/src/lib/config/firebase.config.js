e2fdcc3568a2153d98c27f7ed0f3b020b78eeeda
sabts <sabrinataraschi@gmail.com>
2025-06-09 21:57:53 +0200

    ,

---
+++ client/src/lib/config/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXhdkaxh26FNLy--g9JvMAfSmqji1bZS4",
+  authDomain: "login-chat-b0218.firebaseapp.com",
+  projectId: "login-chat-b0218",
+  storageBucket: "login-chat-b0218.firebasestorage.app",
+  messagingSenderId: "973364137092",
+  appId: "1:973364137092:web:78848829b434d1cbe0c6d1",
+  measurementId: "G-RV5WTZZNQY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
